3092acdd700e79f29d67bf1305b3ef46e2dd9c74
FAVOUR <truelyfavored@gmail.com>
2019-07-28 19:38:14 +0100

    Inetial commit on how to use dagger begining with field injection

--- app/src/main/java/com/example/dagger2/MainActivity.kt
+++ app/src/main/java/com/example/dagger2/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.dagger2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
      lateinit var car:Car
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
