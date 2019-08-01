106b45ec0f2e24307959adfa0df781ff40efa77b
Olije Favour <TRUELYFAVOURED@GMAIL.COM>
2019-08-01 08:16:15 +0100

    Update Remote.java
    Updated the comment

--- app/src/main/java/com/example/dagger2/Remote.java
+++ app/src/main/java/com/example/dagger2/Remote.java
@@ -18,6 +18,8 @@ public class Remote {
 
     }
 
+    
+    //Here Car class serves as te dependency
     public void setListiner(Car car){
 
         Log.d(TAG, "control: Remote Connected..... ");
