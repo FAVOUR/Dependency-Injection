2c4f2e55b77796bb4a44d0e1863eb38e431622c3
FAVOUR <truelyfavored@gmail.com>
2019-08-01 08:37:10 +0100

    Did code clean up in the Car and MainActivity Class

--- app/src/main/java/com/example/dagger2/Car.kt
+++ app/src/main/java/com/example/dagger2/Car.kt
@@ -14,33 +14,13 @@ class Car {
       private  var wheels:Wheels
 
 
-
-
-//      private var engine: Engine
-//
-//    //constructor injection tells dagger to inject this class
-//    @Inject
-//    constructor(wheels: Wheels, engine: Engine) {
-//        this.wheels = wheels
-//        this.engine = engine
-//    }
-
-    //Note that where a constructor is instantiated the field constructor and method are instantiated
-    // correspondingly in that order but the field and method injection can only be done if we do the
-    // constructor injection else you may have to do the field injection as was done in the MainActivity
-
-
-//    private var _engine: Engine
-
-    @Inject lateinit var engine: Engine
-
     //constructor injection tells dagger to inject this class
     @Inject
     constructor(wheels: Wheels) {
 
         this.wheels = wheels
 
-//        _engine = engine
+
 
     }
 
